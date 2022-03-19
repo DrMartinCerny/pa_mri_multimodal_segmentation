f6e556f271e6504c4aae6f5ed4cc69844a60532f
Dr. Martin Cerny <mudr.martin.cerny@gmail.com>
2022-03-19 01:10:51 +0100

    pretrained u-net

--- src/Model.py
+++ src/Model.py
@@ -3,6 +3,7 @@ import tensorflow.keras.backend as K
 from tensorflow_examples.models.pix2pix import pix2pix
 
 from src.KnospScore import KnospScore
+from src.PretrainedModel import unet
 
 class Model:
 
@@ -16,23 +17,44 @@ class Model:
             config.IMG_SIZE+config.ADJACENT_SLICES*2,
             config.NUM_CHANNELS
         ]
+        
+        pretrained_model = unet()
+        pretrained_model.load_weights("data/pretrained_weights.h5")
 
-        base_model = tf.keras.applications.MobileNetV2(input_shape=[config.IMG_SIZE, config.IMG_SIZE, 3], include_top=False)
+        inputs_pretrained = tf.keras.layers.Input(shape=[128,128,32])
+        block_1_convolution_1 = pretrained_model.get_layer('conv2d_2')(inputs_pretrained)
+        block_1_activation_1 = pretrained_model.get_layer('activation_2')(block_1_convolution_1)
+        block_1_convolution_2 = pretrained_model.get_layer('conv2d_3')(block_1_activation_1)
+        block_1_activation_2 = pretrained_model.get_layer('activation_3')(block_1_convolution_2)
+        block_1_max_pooling = pretrained_model.get_layer('max_pooling2d_1')(block_1_activation_2)
+        block_2_convolution_1 = pretrained_model.get_layer('conv2d_4')(block_1_max_pooling)
+        block_2_activation_1 = pretrained_model.get_layer('activation_4')(block_2_convolution_1)
+        block_2_convolution_2 = pretrained_model.get_layer('conv2d_5')(block_2_activation_1)
+        block_2_activation_2 = pretrained_model.get_layer('activation_5')(block_2_convolution_2)
+        block_2_max_pooling = pretrained_model.get_layer('max_pooling2d_2')(block_2_activation_2)
+        block_3_convolution_1 = pretrained_model.get_layer('conv2d_6')(block_2_max_pooling)
+        block_3_activation_1 = pretrained_model.get_layer('activation_6')(block_3_convolution_1)
+        block_3_convolution_2 = pretrained_model.get_layer('conv2d_7')(block_3_activation_1)
+        block_3_activation_2 = pretrained_model.get_layer('activation_7')(block_3_convolution_2)
+        block_3_max_pooling = pretrained_model.get_layer('max_pooling2d_3')(block_3_activation_2)
 
-        # Use the activations of these layers
-        layer_names = [
-            'block_1_expand_relu',   # 64x64
-            'block_3_expand_relu',   # 32x32
-            'block_6_expand_relu',   # 16x16
-            'block_13_expand_relu',  # 8x8
-            'block_16_project',      # 4x4
-        ]
-        base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
+        pretrained_downsampling_stack = tf.keras.Model(inputs=inputs_pretrained, outputs=[block_1_max_pooling,block_2_max_pooling,block_3_max_pooling])
+        pretrained_downsampling_stack.trainable = False
 
-        # Create the feature extraction model
-        down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
+        inputs = tf.keras.layers.Input(shape=[128,128,32])
+        pretrained_output = pretrained_downsampling_stack(inputs)
+        block_4_convolution_1 = tf.keras.layers.Conv2D(256, (3, 3), padding='same')(pretrained_output[2])
+        block_4_activation_1 = tf.keras.layers.LeakyReLU(alpha=0.3)(block_4_convolution_1)
+        block_4_convolution_2 = tf.keras.layers.Conv2D(256, (3, 3), padding='same')(block_4_activation_1)
+        block_4_activation_2 = tf.keras.layers.Activation('relu')(block_4_convolution_2)
+        block_4_max_pooling = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(block_4_activation_2)
+        block_5_convolution_1 = tf.keras.layers.Conv2D(256, (3, 3), padding='same')(block_4_max_pooling)
+        block_5_activation_1 = tf.keras.layers.LeakyReLU(alpha=0.3)(block_5_convolution_1)
+        block_5_convolution_2 = tf.keras.layers.Conv2D(256, (3, 3), padding='same')(block_5_activation_1)
+        block_5_activation_2 = tf.keras.layers.Activation('relu')(block_5_convolution_2)
+        block_5_max_pooling = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(block_5_activation_2)
 
-        down_stack.trainable = False
+        down_stack = tf.keras.Model(inputs=inputs, outputs=[pretrained_output[0],pretrained_output[1],pretrained_output[2],block_4_max_pooling,block_5_max_pooling])
 
         up_stack = [
             pix2pix.upsample(512, 3),  # 4x4 -> 8x8
@@ -46,7 +68,7 @@ class Model:
             embedding = tf.keras.layers.Conv3D(64,3)(inputs)
             embedding = tf.keras.layers.Reshape([config.IMG_SIZE, config.IMG_SIZE, 64])(embedding)
             embedding = tf.keras.layers.LeakyReLU(alpha=0.3)(embedding)
-            embedding = tf.keras.layers.Dense(3)(embedding)
+            embedding = tf.keras.layers.Dense(32)(embedding)
             embedding = tf.keras.layers.LeakyReLU(alpha=0.3)(embedding)
 
             # Downsampling through the model
